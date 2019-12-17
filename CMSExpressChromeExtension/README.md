7eb6e7fbeb4d5beb1aae2ad1f808ebc2cfb10041
keyneslim <vaskeynes@gmail.com>
2019-12-18 00:36:17 +0800

    Add files via upload

---
+++ CMSExpressChromeExtension/README.md
@@ -0,0 +1,7 @@
+Chrome Screenshot 
+=========
+version 1.0
+
+Introduction
+-----------
+Save web page screenshots to image files by one click.
---
+++ CMSExpressChromeExtension/content.js
@@ -0,0 +1,13 @@
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+	
+	if (msg.ready === "ready") {
+		if (confirm('Do you want to capture the screen?')) {
+			sendResponse({download : "download"});
+		}
+	}
+
+	if(msg.ready === "testing"){
+		alert('Testing aaa');
+	}
+		
+}); 
